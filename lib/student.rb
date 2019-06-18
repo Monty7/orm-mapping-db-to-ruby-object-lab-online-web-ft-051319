250e803253e1f0d987c992a070b889e6939e42a0
M Banks <montensia1@outlook.com>
2019-06-18 20:01:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,10 +70,9 @@ class Student
       SELECT * FROM students WHERE grade = 10 LIMIT 1
     SQL
     
-    test = DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
-    binding.pry
   end
   
   def self.all_students_in_grade_X(grade_num)
