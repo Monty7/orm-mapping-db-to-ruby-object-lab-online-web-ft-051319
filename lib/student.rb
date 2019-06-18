1af32f6dc5a17911d7415ed24081203344825ed3
M Banks <montensia1@outlook.com>
2019-06-18 19:37:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,12 +63,12 @@ class Student
     end
   end
   
-  def self.first_X_students_in_grade_10(num_students)
+  def self.first_X_students_in_grade_10
     sql = <<-SQL
-      SELECT * FROM students WHERE grade = 10 LIMIT num
+      SELECT * FROM students WHERE grade = 10 LIMIT 1
     SQL
     
-    DB[:conn].execute(sql, num_students).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
