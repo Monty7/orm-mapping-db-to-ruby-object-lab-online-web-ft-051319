2314840c0aade8bd55b4c3602a0c8af81307b0e4
M Banks <montensia1@outlook.com>
2019-06-18 19:48:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,8 +71,6 @@ class Student
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
-    
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   
   def save
